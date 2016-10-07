5a84c8fe9fcdb6c3aa613620bd470829489633c3
retr0spect <aditya.metalhead@gmail.com>
2016-10-07 00:50:13 -0600

    First App

---
+++ first_app/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: `
+      <h1>Inline Template</h1>
+      <fa-other></fa-other>
+      <fa-another>
+          <div>
+              <h2>Hello</h2>
+              <p>World</p>
+          </div>
+      </fa-another>
+      <fa-another>
+          <h1>Something Else</h1>
+      </fa-another>
+`,
+  styles: [`
+    h1 {
+        color: red;
+    }
+`]
+})
+export class AppComponent {
+  title = 'Aditya';
+}
