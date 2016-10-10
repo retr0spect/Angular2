512347fd92c9528169eabfe8914fd03cebd5871e
Aditya <aditya.chandel@aggiemail.usu.edu>
2016-10-09 20:09:58 -0600

    Databinding

--- first_app/src/app/app.component.ts
+++ first_app/src/app/app.component.ts
@@ -1,25 +1,13 @@
 import { Component } from '@angular/core';
+import { DatabindingComponent } from './databinding/databinding.component';
 
 @Component({
   selector: 'app-root',
-  templateUrl: `
-      <h1>Inline Template</h1>
-      <fa-other></fa-other>
-      <fa-another>
-          <div>
-              <h2>Hello</h2>
-              <p>World</p>
-          </div>
-      </fa-another>
-      <fa-another>
-          <h3>Something Else</h3>
-      </fa-another>
-`,
-  styles: [`
-    h1 {
-        color: red;
-    }
-`]
+  template: `
+      <h1>Root Component</h1>
+      <fa-databinding></fa-databinding>
+`
+
 })
 export class AppComponent {
   title = 'Aditya';
---
+++ first_app/src/app/databinding/databinding.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fa-databinding',
+  templateUrl: './databinding.component.html',
+  styleUrls: ['./databinding.component.css']
+})
+export class DatabindingComponent {
+    stringInterpolation = 'This is string interpolation.';
+    numberInterpolation = 'This is number interpolation.';
+
+    onTest() {
+        return true;
+    }
+
+}
