5187cdb6c60fa76e86422a19f0a1b23b57035c4c
Aditya <aditya.chandel@aggiemail.usu.edu>
2016-10-11 22:57:12 -0600

    DataBinding & Code Formatting

--- recipe-book/src/app/recipes/recipes.component.ts
+++ recipe-book/src/app/recipes/recipes.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Recipe} from "./recipe";
 
 @Component({
   selector: 'rb-recipes',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipesComponent implements OnInit {
 
-  constructor() { }
+  selectedRecipe: Recipe;
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
