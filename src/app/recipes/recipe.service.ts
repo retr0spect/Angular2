d988f1a1ec91a480bfc68803bda932ca4feea39d
Aditya <aditya.chandel@aggiemail.usu.edu>
2016-10-19 00:10:38 -0600

    App Router

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Recipe} from "./recipe";
 import {Ingredient} from "../shared/ingredient";
 
@@ -10,10 +10,15 @@ export class RecipeService {
       new Ingredient('French Fries', 2),
       new Ingredient('Pork Meat', 1)
     ]),
-    new Recipe('Summer Salad', 'Okayish', 'http://ohmyveggies.com/wp-content/uploads/2013/06/the_perfect_summer_salad.jpg', [])
+    new Recipe('Summer Salad', 'Okayish', 'http://ohmyveggies.com/wp-content/uploads/2013/06/the_perfect_summer_salad.jpg', [
+      new Ingredient('Spinach', 2),
+      new Ingredient('Watermelon', 1),
+      new Ingredient('Corinder', 1)
+    ])
   ];
 
-  constructor() { }
+  constructor() {
+  }
 
   getRecipes() {
     return this.recipes;
