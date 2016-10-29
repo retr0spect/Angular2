10bedd32cb7b1ee349c3d5850afddd38cb64e64d
retr0spect <aditya.metalhead@gmail.com>
2016-10-29 00:13:08 -0600

    HTTP Requests

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -3,17 +3,20 @@ import {Recipe} from "../recipe";
 import {RecipeService} from "../recipe.service";
 
 @Component({
-  selector: 'rb-recipe-list',
-  templateUrl: './recipe-list.component.html'
+    selector: 'rb-recipe-list',
+    templateUrl: './recipe-list.component.html'
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [];
+    recipes: Recipe[] = [];
 
-  constructor(private  recipeService: RecipeService) {
-  }
+    constructor(private  recipeService: RecipeService) {
+    }
 
-  ngOnInit() {
-    this.recipes = this.recipeService.getRecipes();
-  }
+    ngOnInit() {
+        this.recipes = this.recipeService.getRecipes();
+        this.recipeService.recipesChanged.subscribe(
+            (recipes: Recipe[]) => this.recipes = recipes
+        );
+    }
 
 }
